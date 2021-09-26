21b7d9d9172cb4a72aeceb4c256fd584080318f4
kexinyu <kexinyu@umich.edu>
2021-09-26 15:23:02 -0400

    first attempt

--- hw4.py
+++ hw4.py
@@ -28,7 +28,8 @@ class Customer:
     # Submit_order takes a cashier, a stall and an amount as parameters, 
     # it deducts the amount from the customer’s wallet and calls the receive_payment method on the cashier object
     def submit_order(self, cashier, stall, amount): 
-        pass
+        self.wallet -= amount
+        cashier.receive_payment(stall, amount)
 
     # The __str__ method prints the customer's information.    
     def __str__(self):
@@ -71,9 +72,38 @@ class Cashier:
 
 ## Complete the Stall class here following the instructions in HW_4_instructions_rubric
 class Stall:
-    
-    pass
 
+    def __init__(self, name, inventory, cost = 7, earnings = 0):
+        self.name = name
+        self.inventory = inventory.copy()
+        self.cost = cost
+        self.earnings = earnings
+
+    
+    def process_order(self, name, quantity):
+        if self.has_item(name, quantity):
+            self.inventory[name] -= quantity
+        else:
+            return "Sorry our stall don't have enough " + name
+    
+    def has_item(self, name, quantity):
+        if self.inventory[name] >= quantity:
+            return True
+        else:
+            return False
+
+    def stock_up(self, name, quantity):
+        if name in self.inventory:
+            self.inventory[name] += quantity
+        else:
+            self.inventory[name] = quantity
+        
+    def compute_cost(self,quantity):
+        total = self.cost * quantity
+        return total
+
+    def _str__(self):
+        return "Hello, we are " + self.name + ". This is the current menu " + self.inventory.keys() + ". We charge $" + str(self.cost) + "per item." + " We have $" + str(self.earnings) + " in total." 
 
 class TestAllMethods(unittest.TestCase):
     
